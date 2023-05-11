34db41c75a09b3947b4d55544697549a8605a690
Tapos Roy <89276137+Developertaposroy@users.noreply.github.com>
2023-05-11 21:52:19 +0600

    Module-6
    Module-6

--- Module-6/server.js
+++ Module-6/server.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
-// Set up body-parser middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Array to store books
-let books = [];
-
-// Serve static index.html file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// GET route to fetch all books
+
+let books = [];
+
 app.get('/books', (req, res) => {
     res.json(books);
 });
 
-// POST route to add a new book
+
 app.post('/books', (req, res) => {
     const { title, author, publishedDate } = req.body;
     const book = {
@@ -33,19 +31,20 @@ app.post('/books', (req, res) => {
     res.json(book);
 });
 
-// DELETE route to remove a book
+
 app.delete('/books/:id', (req, res) => {
     const { id } = req.params;
-    const index = books.findIndex(book => book.id === id);
-    if (index !== -1) {
-        books.splice(index, 1);
+    const bookIndex = books.findIndex(book => book.id === id);
+    if (bookIndex !== -1) {
+        books.splice(bookIndex, 1);
         res.json({ message: 'Book deleted successfully' });
     } else {
         res.status(404).json({ message: 'Book not found' });
     }
 });
 
+
 const port = 3000;
-app.listen(port,() => {
-    console.log(`Server started`);
+app.listen(port, () => {
+    console.log(`Server Run Success`);
 });
